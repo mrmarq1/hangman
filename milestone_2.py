fc7188afac6bcfb88cec6d7b78ad369f2c90a353
Matthew Marquis <matthew_marquis@btinternet.com>
2023-03-04 13:11:26 +0000

    Added user input validation

--- milestone_2.py
+++ milestone_2.py
@@ -5,3 +5,8 @@ word = random.choice(word_list)
 print(word)
 
 guess = input('Please enter a single letter: ')
+if len(guess) == 1 and guess.isalpha():
+    print('Good guess!')
+else:
+    print('Oops! That is not a valid input.')
+    
