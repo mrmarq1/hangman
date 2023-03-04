974d8e4fb9fa7b610294a6932d04a48e90f3491b
mrmarq1 <126958930+mrmarq1@users.noreply.github.com>
2023-03-04 21:22:52 +0000

    Added functions to class (#19)

--- milestone_4.py
+++ milestone_4.py
@@ -8,3 +8,22 @@ class Hangman:
         self.word_guessed = ['_']*len(self.word)
         self.num_letters = len(set(self.word))
         self.list_of_guesses = []
+
+        def check_guess(guess):
+            guess = guess.lower()
+            if guess in self.word:
+                print(f'Good guess! {guess} is in the word.')
+
+        def ask_for_input():
+            while True:
+              guess = input('Please enter a single letter: ')
+              if len(guess) != 1 or not guess.isalpha():
+                  print('Invalid letter. Please, enter a single alphabetical character.')
+              elif guess in self.list_of_guesses:
+                  print('You already tried that letter!')
+              else:
+                  check_guess(guess)
+                  self.list_of_guesses.append(guess)
+                  break
+
+        ask_for_input()
