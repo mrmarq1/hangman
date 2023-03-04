e3e8c46735c7a47468d846d603d071b31ce53d5a
Matthew Marquis <matthew_marquis@btinternet.com>
2023-03-04 17:53:16 +0000

    Recommiting refactoring to functions and user guess to lowercase

--- milestone_3.py
+++ milestone_3.py
@@ -4,15 +4,22 @@ word_list = ['apples','pears','kiwis','pomegranates','strawberries']
 word = random.choice(word_list)
 print(word)
 
-while True:
-  guess = input('Please enter a single letter: ')
-  if len(guess) == 1 and guess.isalpha():
-      break
-  else:
-      print('Invalid letter. Please, enter a single alphabetical character.')
+def ask_for_input():
+  while True:
+    guess = input('Please enter a single letter: ')
+    if len(guess) == 1 and guess.isalpha():
+        break
+    else:
+        print('Invalid letter. Please, enter a single alphabetical character.')
+  
+  check_guess(guess)
 
-if guess in word:
-   print(f'Good guess! {guess} is in the word.')
-else:
-   print(f'Sorry, {guess} is not in the word. Try again')
+def check_guess(guess):
+  guess_lower = guess.lower()
+  if guess_lower in word:
+    print(f'Good guess! {guess_lower} is in the word.')
+  else:
+    print(f'Sorry, {guess_lower} is not in the word. Try again')
+
+ask_for_input()
    
