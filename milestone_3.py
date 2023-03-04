221da83e7587e94c72fc10f60c3eec23e2e9b3e8
Matthew Marquis <matthew_marquis@btinternet.com>
2023-03-04 17:59:35 +0000

    Commit to check whether guess in word

--- milestone_3.py
+++ milestone_3.py
@@ -4,22 +4,15 @@ word_list = ['apples','pears','kiwis','pomegranates','strawberries']
 word = random.choice(word_list)
 print(word)
 
-def ask_for_input():
-  while True:
-    guess = input('Please enter a single letter: ')
-    if len(guess) == 1 and guess.isalpha():
-        break
-    else:
-        print('Invalid letter. Please, enter a single alphabetical character.')
-  
-  check_guess(guess)
-
-def check_guess(guess):
-  guess_lower = guess.lower()
-  if guess_lower in word:
-    print(f'Good guess! {guess_lower} is in the word.')
+while True:
+  guess = input('Please enter a single letter: ')
+  if len(guess) == 1 and guess.isalpha():
+      break
   else:
-    print(f'Sorry, {guess_lower} is not in the word. Try again')
+      print('Invalid letter. Please, enter a single alphabetical character.')
 
-ask_for_input()
+if guess in word:
+   print(f'Good guess! {guess} is in the word.')
+else:
+   print(f'Sorry, {guess} is not in the word. Try again')
    
