ff4524bdbdb5fdf8bdca7db29b53350b3c5fc10d
Matthew Marquis <matthew_marquis@btinternet.com>
2023-03-05 09:40:44 +0000

    Added Hangman class

--- milestone_4.py
+++ milestone_4.py
@@ -5,6 +5,6 @@ class Hangman:
         self.word_list = word_list
         self.num_lives = num_lives
         self.word = random.choice(word_list)     
-        self.word_guessed = ['_']*len(self.word)
+        self.word_guckessed = ['_']*len(self.word)
         self.num_letters = len(set(self.word))
         self.list_of_guesses = []
